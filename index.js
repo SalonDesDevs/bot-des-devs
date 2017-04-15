2ef6a407920e59b2cf8ca11d8d60b0cc67dd5180
FliiFe <theophile.cailliau@gmail.com>
2017-04-15 18:53:54 +0200

    Add format checking for #liens

---
+++ index.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const {
+    CommandManager
+} = require('krobotjs');
+
+const bot = new Discord.Client();
+const commands = new CommandManager({
+    parse: [{
+        // Match user (<@ID>)
+        match(message, arg) {
+            return (/^<@((\d)+)>$/g).test(arg)
+        },
+        // Replace it with an instance of Discord.User(ID)
+        async perform(message, arg) {
+            const gmember = await message.channel.guild.fetchMember((/^<@((\d)+)>$/g).exec(arg)[1]);
+            return gmember.user;
+        }
+    }]
+});
+
+// Create a command group
+commands.group().prefix("#").apply(_ => {
+    commands
+        .command("test <message> <nb:^[0-9]+$> [opt]", (message, args) => message.reply('test')).register()
+        .sub("moche", (message, args) => message.reply('t es moche')).register();
+    commands
+        .command("lol <message>", (message, args) => message.reply('lol')).register()
+        .sub("test", (message, args) => message.reply('lol test')).register();
+    commands
+        .command("test2 <message>", (message, args) => message.reply(args.get('message'))).register()
+        .sub("moche", (message, args) => message.reply('t es moche 2')).register();
+});
+
+// The regexp can both validate the format and extract the contents.
+// The second match is the Category, the third is the Description, and the fourth contains
+// the urls.
+const linkFormatRegexp = /^ ?\[((?:\*\*)?)([^*]+?)\1\] *([\w\W]+?) *[\n:]+ *(https?:\/\/[\w\W]+)$/i;
+
+bot.on('message', message => {
+    // If the message is a command, then it will be executed and will return "true"
+    // yet, if there is no command, it will return "false"
+    if (!commands.dispatch(message)) {
+        let channel = message.channel;
+        if(channel.name === 'liens') {
+            console.log(message.content);
+            console.log('New message in #liens, checking for formatting errors');
+            let matches = message.content.match(linkFormatRegexp) || [];
+            if(matches.length === 0) {
+                console.log('Message incorectly formatted. Deleting');
+                message.delete();
+                console.log('Sending deleted message to its author');
+                message.author.send('Le lien que vous avez envoyé dans #liens est mal formatté !');
+                message.author.send('Pour rappel, vous devez utiliser la syntaxe: `[**Catégorie**] Description: lien`');
+                message.author.send('Voici le message que vous avez envoyé :');
+                message.author.send(message.content);
+            } else {
+                console.log('Message is correctly formatted ! Yay !');
+                console.log('Category:', matches[2]);
+                console.log('Description:', matches[3]);
+                console.log('Link(s):', matches[4].split(/\s/g));
+            }
+        }
+    }
+    // continue actions
+});
+bot.on('ready', _ => console.log('Connected'));
+bot.on('reconnecting', _ => console.log('Reconnecting'));
+bot.on('error', error => console.error(error));
+
+bot.login(process.env.DISCORD_TOKEN);
